f82986193605a8d7c19eca8e7dfff35b238a62f8
aditya17092001 <adityaudata@gmail.com>
2025-06-28 23:37:15 +0530

    1200 Rating

--- B_Serval_and_Final_MEX.java
+++ B_Serval_and_Final_MEX.java
@@ -18,24 +18,24 @@ public class B_Serval_and_Final_MEX{
         Queue<Integer> q = new LinkedList<>();
 
         int i = 0;
-        for(i = 0; i < n; i++) {
-            if(a[i] == 0) {
+        for(i = 0; i < n-1; i+=2) {
+            if(a[i] == 0 || a[i+1] == 0) {
                 int start = i-skipped;
                 q.add(start);
-                while(i < n && a[i] == 0) i++;
-                if(i == n && (start+skipped) != (i-1)) {
-                    q.add(i-1-skipped);
-                    skipped = skipped + ((i-1-skipped) - start);
-                }
-                else if((start+skipped) != (i-1)){
-                    q.add(i-skipped);
-                    skipped = skipped + ((i-skipped) - start);
-                }
+                q.add(i+1-skipped);
+                skipped++;
             }
         }
 
+        if((n&1) != 0 && a[n-1] == 0) {
+            q.add(n-2-skipped);
+            q.add(n-1-skipped);
+            skipped++;
+        }
+
+
+        System.out.println(q.size()/2+1);
         while(!q.isEmpty()) System.out.println((q.poll()+1)+" "+(q.poll()+1));
         System.out.println(1+" "+(n-skipped));
     }
 }
-
