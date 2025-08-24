e2d06bc06763e4e62c4a141d6dd71315acb2ece2
aditya17092001 <adityaudata@gmail.com>
2025-08-24 21:23:50 +0530

    1200 B_Bowling_Frame

---
+++ B_Bowling_Frame.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class B_Bowling_Frame {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        while(t-- > 0) {
+            long w = s.nextLong();
+            long b = s.nextLong();
+            solve(w, b);
+        }
+    }
+
+    public static void solve(long w, long b) {
+        long ans = 0, tans = 0;
+
+        long i = 1, j = (long) Math.pow(10, 9);
+        while(i <= j) {
+            long mid = (i + j)/2;
+
+            if(w+b >= (mid * (mid+1))/2) {
+                tans = mid;
+                i = mid+1;
+            } else j = mid - 1;
+        }
+
+        System.out.println(tans);
+    }
+}
+
